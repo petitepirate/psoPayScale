44de11d0f3a1ce50e25f4730038ddb0294d95103
megan <msmeganmcmanus@gmail.com>
2020-10-17 20:51:20 -0700

    add regions

--- app.py
+++ app.py
@@ -49,3 +49,63 @@ def add_user():
 @app.route("/user/info", methods=["GET"])
 def user_page():
     return render_template('userinfo.html')
+
+@app.route("/ocean/1", methods=["GET"])
+def ocean1_page():
+    return render_template('id1.html')
+    
+@app.route("/ocean/2", methods=["GET"])
+def ocean2_page():
+    return render_template('id2.html')
+
+@app.route("/ocean/3", methods=["GET"])
+def ocean3_page():
+    return render_template('id3.html')
+
+@app.route("/ocean/4", methods=["GET"])
+def ocean4_page():
+    return render_template('id4.html')
+
+@app.route("/ocean/5", methods=["GET"])
+def ocean5_page():
+    return render_template('id5.html')
+
+@app.route("/ocean/6", methods=["GET"])
+def ocean6_page():
+    return render_template('id6.html')
+
+@app.route("/ocean/7", methods=["GET"])
+def ocean7_page():
+    return render_template('id7.html')
+
+@app.route("/ocean/8", methods=["GET"])
+def ocean8_page():
+    return render_template('id8.html')
+
+@app.route("/ocean/9", methods=["GET"])
+def ocean9_page():
+    return render_template('id9.html')
+
+@app.route("/ocean/10", methods=["GET"])
+def ocean10_page():
+    return render_template('id10.html')
+
+@app.route("/ocean/11", methods=["GET"])
+def ocean11_page():
+    return render_template('id11.html')
+
+@app.route("/ocean/12", methods=["GET"])
+def ocean12_page():
+    return render_template('id12.html')
+
+@app.route("/ocean/13", methods=["GET"])
+def ocean13_page():
+    return render_template('id13.html')
+
+@app.route("/ocean/14", methods=["GET"])
+def ocean14_page():
+    return render_template('id14.html')
+
+@app.route("/ocean/15", methods=["GET"])
+def ocean15_page():
+    return render_template('id15.html')
